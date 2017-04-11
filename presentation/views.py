9a5e6d090a7259bef840eb86cae027da5d191b5d
Artem Zagurskiy <zuznik@mail.ru>
2017-04-11 16:09:38 +0300

    Changed HTML for the site

--- presentation/views.py
+++ presentation/views.py
@@ -3,4 +3,5 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+def post_list(request):
+    return render(request, 'presentation/post_list.html', {})
